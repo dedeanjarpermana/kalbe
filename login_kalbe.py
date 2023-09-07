000559346931c6cfb5de67e5b31909e758b5be80
dedeanjarpermana <63705330+dedeanjarpermana@users.noreply.github.com>
2023-09-07 21:14:56 +0700

    automation kalbe update

--- login_kalbe.py
+++ login_kalbe.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+import time  # Impor modul time
 
 # driver untuk menambahkan waktu tunggu
 from selenium.webdriver.common.by import By
@@ -10,25 +11,20 @@ from selenium.webdriver.support import expected_conditions as EC
 service = Service(executable_path='./chromedriver.exe')
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
-# ...
-
-
-
 
 # Buka URL
 url = "http://103.16.199.79:5000/login"
 driver.get(url)
+time.sleep(3)
 
-
-wait = WebDriverWait(driver, 3)  # Tunggu hingga 20 detik
-elemen = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div/form/div/div/div[4]/input")))
+# wait = WebDriverWait(driver, 3)  # Tunggu hingga 20 detik
+# elemen = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div/form/div/div/div[4]/input")))
 # Isi kolom Username dan Password
 username = driver.find_element("id", 'email')
 password = driver.find_element("id", 'password')
 
 # username = driver.find_element("name", 'username')
 # password = driver.find_element("name", 'password')
-
 
 #cara gini ga bisa
 # username = driver.find_element_by_class_name('oxd-input oxd-input--focus')
@@ -39,23 +35,27 @@ password = driver.find_element("id", 'password')
 # password= driver.find_element("xpath", '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input')
 # username = driver.find_element("xpath", '/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input')
 # password = driver.find_element("xpath", '/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input')
+time.sleep(3)
 username.send_keys("admin@mail.com")
+
+time.sleep(3)
 password.send_keys("12345")
 
-
-
-
 # Submit formulir login
+time.sleep(3)
 password.send_keys(Keys.RETURN)
 
 # Tunggu beberapa saat untuk memastikan halaman terbuka
 driver.implicitly_wait(15)
 
+dashboard = driver.find_element("xpath", '/html/body/div[2]/div[1]/div[2]/div/ul/li[2]/a/span')
+time.sleep(2)
+dashboard.click()
 # Verifikasi apakah login berhasil
-if "dashboard" in driver.current_url:
-    print("Login berhasil!")
-else:
-    print("Login gagal!")
+# if "a_Dashboard" in driver.current_url:
+#     print("Login berhasil!")
+# else:
+#     print("Login gagal!")
 
 # Tutup browser
 driver.quit()
